55574b6a95e0b7a0428d2e87e86b8eed03383310
Alok Gaira <alok.gaira@rksv.in>
2017-09-23 16:41:09 +0530

    [Feature :] : Added  body parser.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
